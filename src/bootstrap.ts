542e78acbf2813f519d4a342214a42e697078f48
Pavlo Baliuk <pv.baliuk@gmail.com>
2026-01-30 14:56:14 +0200

    Fix bootstrap.ts

--- src/bootstrap.ts
+++ src/bootstrap.ts
@@ -2,5 +2,4 @@ import 'module-alias/register';
 import {loadTsconfigPaths} from './helpers/utils/load-tsconfig-paths';
 import {join} from 'path';
 
-const _path = __dirname.split(/[\\\/]src/i)[0];
-loadTsconfigPaths(join(_path, 'src'), join(_path, 'tsconfig.json'));
+loadTsconfigPaths(__dirname, join(__dirname, '..', 'tsconfig.json'));
